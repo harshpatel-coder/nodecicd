06777b04dd0ba13f2c2e4e9bcd0f1e30d5ffef0d
harshpatel-coder <harshpatel@hyperlinkinfosystem.com>
2026-04-29 17:00:02 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,15 +2,19 @@
 
 echo "🚀 Deployment started..."
 
+# Fix permission
 sudo chown -R ubuntu:ubuntu /var/www/html
 
 cd /var/www/html || exit
 
-npm install
+echo "📦 Installing dependencies..."
+npm install --unsafe-perm
 
+echo "🛑 Stopping old app..."
 pm2 stop test-app || true
 pm2 delete test-app || true
 
+echo "🚀 Starting app..."
 pm2 start index.js --name test-app
 
 pm2 save
