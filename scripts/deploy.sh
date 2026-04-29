ea172aff04a7d9e4a9e3f49aa24a9cd1d92d26a3
harshpatel-coder <harshpatel@hyperlinkinfosystem.com>
2026-04-29 16:40:50 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-cd /var/www/html
+echo "🚀 Deployment started..."
 
-npm install
+# Go to app directory
+cd /var/www/html || exit
 
-pm2 restart all || pm2 start app.js
+echo "📦 Installing dependencies..."
+npm install --production
 
-sudo systemctl restart apache2
+echo "🛑 Stopping old app..."
+pm2 stop node-hello || true
+pm2 delete node-hello || true
+
+echo "🧹 Cleaning old logs..."
+pm2 flush
+
+echo "🚀 Starting app..."
+pm2 start index.js --name node
+
+echo "💾 Saving pm2 process..."
+pm2 save
+
+echo "🔁 Restarting pm2 startup (optional)"
+pm2 startup systemd -u ubuntu --hp /home/ubuntu
+
+echo "✅ Deployment finished successfully!"
