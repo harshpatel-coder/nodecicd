991c0ca6e87100cd0686289e6f9314fb04ea582b
harshpatel-coder <harshpatel@hyperlinkinfosystem.com>
2026-05-04 18:02:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'hello from;;; harsh patel!!\n'
+  const msg = 'hello!!\n'
   res.end(msg);
 });
 
